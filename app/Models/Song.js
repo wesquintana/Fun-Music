11b471d5e9a30170bc8017a6fcb28f2e3f9c11f5
wesquintana <57374405+wesquintana@users.noreply.github.com>
2019-11-26 12:44:38 -0700

    Initial commit

---
+++ app/Models/Song.js
@@ -0,0 +1,24 @@
+export default class Song {
+  constructor(data) {
+    this.title = data.trackName || data.title;
+    this.albumArt =
+      data.albumArt || data.artworkUrl100.replace(/100x100/g, "300x300");
+    this.artist = data.artistName || data.artist;
+    this.album = data.collectionName || data.album;
+    this.price = data.trackPrice || data.price;
+    this.preview = data.previewUrl || data.preview;
+    this._id = data.trackId || data._id;
+  }
+
+  get Template() {
+    return `
+
+        `;
+  }
+
+  get playlistTemplate() {
+    return `
+
+        `;
+  }
+}
